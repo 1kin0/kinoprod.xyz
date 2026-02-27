666ca4f1e76cd2d36b1b42af3d670dcf48f7be64
kinovw <67692844+kinovw@users.noreply.github.com>
2026-02-27 18:18:52 +0300

    text width update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,20 +1,18 @@
 const NotFound = () => {
     return (
         <>
-            <div id="content" className='w-lvw h-lvh flex items-center justify-center'>
+            <div id="content" className='w-lvw h-lvh flex flex-col items-center justify-center'>
                 <style jsx>{`
                     .rotate {
                         animation: rotate 16s linear infinite;
-                        z-index: -1;
-                        filter: opacity(0.1);
                     }
                     @keyframes rotate {
                         from { transform: rotate(0deg); }
                         to { transform: rotate(360deg); }
                     }
                 `}</style>
-                <img src="sun.png" alt="Logo" className="rotate w-3xs absolute"/>
-                <h1 className="text-9xl italic">404 NOT FOUND</h1>
+                <h1 className="text-9xl italic font-bold">404 NOT FOUND</h1>
+                <img src="sun.png" alt="Logo" className="rotate w-22"/>
             </div>
         </>
     )
